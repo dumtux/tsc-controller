a351b20d8ee6bc5cd223ef5ee6582123a2931e7c
BadTux <tekobase@outlook.com>
2022-11-27 15:50:54 +0800

    update relay example.

--- software/spike.py
+++ software/spike.py
@@ -5,16 +5,21 @@ import digitalio
 from adafruit_mcp230xx.mcp23017 import MCP23017
 
 i2c = busio.I2C(board.SCL, board.SDA)
-mcp = MCP23017(i2c, address=0x20)  # MCP23017 w/ A0 set
-pin0 = mcp.get_pin(0)
-# pin1 = mcp.get_pin(1)
-pin0.switch_to_output(value=True)
-# pin1.direction = digitalio.Direction.INPUT
-# pin1.pull = digitalio.Pull.UP
+mcp = MCP23017(i2c, address=0x20)
+
+relay6 = mcp.get_pin(13)  # Port B 5
+relay7 = mcp.get_pin(14)  # Port B 6
+relay6.switch_to_output(value=True)
+relay7.switch_to_output(value=True)
+relay6.direction = digitalio.Direction.OUTPUT
+relay7.direction = digitalio.Direction.OUTPUT
 
 while True:
-    pin0.value = True
-    time.sleep(0.5)
-    pin0.value = False
-    time.sleep(0.5)
-    # print("Pin 1 is at a high level: {0}".format(pin1.value))
+    relay6.value = True
+    time.sleep(1)
+    relay6.value = False
+    time.sleep(1)
+    relay7.value = True
+    time.sleep(1)
+    relay7.value = False
+    time.sleep(1)
